e49cdc29eb56fffff83246433487d35a7efea11d
Markus Tinnfalt <mctinn@gmail.com>
2018-11-16 17:50:16 +0100

    wip: player cast mechanic

--- medieval-fishing-game/Assets/Player/CastPreview.cs
+++ medieval-fishing-game/Assets/Player/CastPreview.cs
@@ -6,14 +6,30 @@ public class CastPreview : MonoBehaviour
 {
 	public CastMeter castMeter;	
 	private LineRenderer lineRenderer;
+	[SerializeField]
+	private Vector3[] positions;
+
 
 	void Start ()
 	{
 		lineRenderer = GetComponent<LineRenderer>();
+		positions = new Vector3[10];
 	}
 
 	void Update ()
 	{
-		lineRenderer.SetPosition(1, new Vector3(0, castMeter.accuracy * 10, castMeter.velocity * 10));
+
+		float t = 0.0f;
+		for(int i =0; i < positions.Length; i++) {
+			t = i * 1f;
+			positions[i].x = 5.0f * t;
+			var tt = t + 10.0f;
+			positions[i].y = 1.0f - tt * tt;
+			positions[i].z = 0.0f;
+		}
+		// print(lineRenderer.positionCount);
+
+		lineRenderer.SetPositions(positions);
+		//lineRenderer.SetPosition(1, new Vector3(0, castMeter.accuracy * 10, castMeter.velocity * 10));
 	}
 }
