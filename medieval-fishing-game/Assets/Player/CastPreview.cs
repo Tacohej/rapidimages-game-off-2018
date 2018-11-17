c660cf331aa7c302b10bcc6add4618ebcc583998
Martin Nilsson <martin.lennart.nilsson@gmail.com>
2018-11-17 15:53:24 +0100

    Update the cast arc preview.

--- medieval-fishing-game/Assets/Player/CastPreview.cs
+++ medieval-fishing-game/Assets/Player/CastPreview.cs
@@ -2,34 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CastPreview : MonoBehaviour
-{
-	public CastMeter castMeter;	
-	private LineRenderer lineRenderer;
-	[SerializeField]
-	private Vector3[] positions;
+public class CastPreview : MonoBehaviour {
+    public CastMeter castMeter;
+    private LineRenderer lineRenderer;
+    [SerializeField]
+    private Vector3[] positions;
 
+    public float starting_height = 1.0f;
+    public float initial_velocity = 20.0f;
+    public float air_resistance = 2.0f;
+    private float gravity = -10;
+    private const int segments = 32;
 
-	void Start ()
-	{
-		lineRenderer = GetComponent<LineRenderer>();
-		positions = new Vector3[10];
-	}
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        positions = new Vector3[segments];
+        lineRenderer.positionCount = segments;
+        lineRenderer.startWidth = 2.0f;
+        lineRenderer.endWidth = 0.1f;
+    }
 
-	void Update ()
-	{
+    void Update () {
 
-		float t = 0.0f;
-		for(int i =0; i < positions.Length; i++) {
-			t = i * 1f;
-			positions[i].x = 5.0f * t;
-			var tt = t + 10.0f;
-			positions[i].y = 1.0f - tt * tt;
-			positions[i].z = 0.0f;
-		}
-		// print(lineRenderer.positionCount);
+        float t = 0.0f;
+        float impact_t = calculate_impact_time();
+        float step = impact_t / (positions.Length - 1);
+        for(int i=0; i < positions.Length; i++) {
+            t = i * step;
+            var tt = t * t;
+            positions[i] = calculate_arc_position(t);
+        }
 
-		lineRenderer.SetPositions(positions);
-		//lineRenderer.SetPosition(1, new Vector3(0, castMeter.accuracy * 10, castMeter.velocity * 10));
-	}
+        lineRenderer.SetPositions(positions);
+    }
+
+    private Vector3 calculate_arc_position(float t) {
+        var tt = t * t;
+        Vector3 v;
+        v.x = initial_velocity * t - air_resistance * tt;
+        v.y = starting_height + initial_velocity * t + gravity * tt;
+        v.z = 0;
+        return v;
+    }
+
+    private float calculate_impact_time() {
+        var center = initial_velocity / (2.0f * gravity);
+        var offset = Mathf.Sqrt(center * center - starting_height / gravity);
+        return -center + Mathf.Abs(offset);
+    }
 }
