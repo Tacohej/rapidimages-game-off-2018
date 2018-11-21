805a473d94e72a1b70c9ad85af745004b6ffa924
Anton Bergman <abergman@rapidimages.se>
2018-11-21 18:21:26 +0100

    Add scripts for changing between scenes, added main menu scene with cute background

---
+++ medieval-fishing-game/Assets/Scenes/MainMenuSceneHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSceneHandler : MonoBehaviour 
+{
+	void Start () 
+	{
+		SceneHandler.Init ();
+	}
+
+    public void ChangeScene(string sceneName){
+        SceneHandler.SwitchScene(sceneName);
+    }
+}
---
+++ medieval-fishing-game/Assets/Scenes/SceneHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+//using UnityEditor;
+
+/// <summary>
+/// Class containing methods for changing and exiting the current scene.
+/// </summary>
+public class SceneHandler
+{
+	private static bool alreadyInitialized = false;
+	private static Stack sceneStack;
+	private static AssetBundle myLoadedAssetBundle;
+	private static string[] scenePaths;
+	private static ArrayList scenePathsList;
+
+	public static void Init()
+	{
+		if (!alreadyInitialized)
+		{
+			scenePathsList = new ArrayList ();
+			sceneStack = new Stack ();
+
+			scenePathsList.Add("SampleScene");
+			scenePathsList.Add("MainMenuScene");
+		}
+	}
+
+	public static void SwitchScene(string sceneName)
+	{
+		if (scenePathsList.Contains (sceneName))
+		{
+			sceneStack.Push (SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+
+	public static void GoBackToScene(string sceneName)
+	{
+		//Pop stack until a matching scene is found.
+		while (!sceneName.Equals ((string)sceneStack.Peek ()) && sceneStack.Count > 1)
+		{
+			sceneStack.Pop ();
+		}
+		string newSceneName = (string)sceneStack.Pop ();
+		if (scenePathsList.Contains(newSceneName))
+		{
+			SceneManager.LoadScene(newSceneName);
+		}
+	}
+}
