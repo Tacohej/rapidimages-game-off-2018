e7818157cbfe9a156d589147f70596588559c802
Joakim Ihlström <6200km@gmail.com>
2018-11-29 07:57:40 +0100

    Add a simple UI

---
+++ medieval-fishing-game/Assets/Inventory/InventoryItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public abstract class InventoryItem : ScriptableObject {
+	public string title;
+	public string flavorText;
+	public Sprite diplayImage;
+	[NonSerialized] public bool found;
+}
+
+[CreateAssetMenu (menuName = "MedievalFishing/Inventory/Rod")]
+public class RodItem : InventoryItem {
+
+}
+
+[CreateAssetMenu (menuName = "MedievalFishing/Inventory/Bait")]
+public class BaitItem : InventoryItem {
+
+}
---
+++ medieval-fishing-game/Assets/Inventory/SetFirstSelectedBait.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFirstSelectedBait : MonoBehaviour {
+
+	public Inventory inventory;
+	public BaitItem bait;
+
+	void Start () {
+		bait.found = true;
+		inventory.SelectedBait = bait;
+	}
+}
